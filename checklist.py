6f483f04f0894305cfc864799c125f5373643ccc
Ivan Yerkinov <ivan@Ivans-MacBook-Pro.local>
2019-09-02 12:13:13 -0700

    Finished Checklist.py

--- checklist.py
+++ checklist.py
@@ -21,7 +21,49 @@ def destroy(index):
     checklist.pop(index)
 
 
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print("{} {}".format(index, list_item))
+        index += 1
+
+
+def mark_completed(index):
+    checklist[index] = "\xe2" + checklist[index]
+    list_all_items()
+
+
+def user_input(prompt):
+    user_i = input(prompt)
+    return user_i
+
+
+def select(function_code):
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+    elif function_code == "R":
+        item_index = int(user_input("Index Number?"))
+        if(item_index < len(checklist)):
+            print(read(item_index))
+        else:
+            print("Enter a valid index number.")
+            print("Current index size: " + str(len(checklist)))
+    elif function_code == "P":
+        list_all_items()
+
+    elif function_code == "Q":
+        return False
+
+    else:
+        print("Unkown Option")
+
+    return True
+
+
 def test():
+    user_value = user_input("Please enter a value:")
+    print(user_value)
     create("purple sox")
     create("red cloak")
 
@@ -29,7 +71,18 @@ def test():
     print(read(1))
 
     update(0, "purple socks")
-    destory(1)
+
+    destroy(1)
 
     print(read(0))
-    print(read(1))
+
+
+test()
+
+running = True
+while running:
+    selection = user_input(
+    "Press C to add to list, R to read from the list, P to display the list and Q to quit.\n"
+    )
+    print(chr(27) + "[2J")
+    running = select(selection)
